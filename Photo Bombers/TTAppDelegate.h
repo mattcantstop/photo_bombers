66c25c707f5d3306bfe52281b3ab2c000f086c08
Matt Duff <matt.duff@moneydesktop.com>
2014-05-20 08:35:22 -0600

    Initial Commit

---
+++ Photo Bombers/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  Photo Bombers
+//
+//  Created by Duff on 5/20/14.
+//  Copyright (c) 2014 tiniestTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
